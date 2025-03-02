30ba572a661a89dbb37e955d1b82ab0d542c3c02
Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>
2025-03-02 09:23:02 +0100

    Removed Applications

--- Tests/Unit/Internal/MockApplication.cs
+++ Tests/Unit/Internal/MockApplication.cs
@@ -1,6 +1,4 @@
-﻿using FluentInjections.Application;
-
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.Extensions.DependencyInjection;
