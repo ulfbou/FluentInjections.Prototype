78c45dc6e782ec8c34536d724b029edabf38c7f8
Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>
2025-03-14 15:33:39 +0100

    Updated Core.DependencyInjection

---
+++ Src/FluentInjections/DependencyInjection/IAsyncServiceProvider.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) FluentInjections Project. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+
+namespace FluentInjections.DependencyInjection
+{
+    public interface IAsyncServiceProvider : IAsyncDisposable
+    {
+        ValueTask<object?> GetServiceAsync(Type serviceType, CancellationToken cancellationToken = default);
+    }
+}
