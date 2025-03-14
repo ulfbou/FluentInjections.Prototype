6e8a0a0919269e9e51d832b609afbc961a9622ad
Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>
2025-03-14 15:58:30 +0100

    Initial commit FluentInjections.Utilities.Collections

---
+++ Src/FluentInjections/Utilities/Collections/CompositeDictionary{TKey,TValue}.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) FluentInjections Project. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace FluentInjections.Utilities.Collections
+{
+    public sealed class CompositeDictionary<TKey, TValue> where TKey : notnull
+    {
+        private readonly List<IDictionary<TKey, TValue>> _sources = new();
+
+        public void AddSource(IDictionary<TKey, TValue> source) => _sources.Add(source);
+
+        public bool TryGetValue(TKey key, out TValue? value)
+        {
+            foreach (var source in _sources)
+            {
+                if (source.TryGetValue(key, out value))
+                    return true;
+            }
+
+            value = default;
+            return false;
+        }
+    }
+}
