8084f26963bf84aab6dbff51c57b26634766d43d
Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>
2025-03-14 15:32:41 +0100

    Updated Core.Configuration

---
+++ Src/FluentInjections/Core/Configuration/TypeDiscoveryOptions.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) FluentInjections Project. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using FluentInjections.Abstractions;
+using FluentInjections.Core.Discovery.Metadata;
+
+using Microsoft.AspNetCore.Builder;
+
+namespace FluentInjections.Core.Configuration
+{
+    public class TypeDiscoveryOptions
+    {
+        public static TypeDiscoveryOptions DefaultOptions = new TypeDiscoveryOptions
+        {
+            AssemblyFilterPredicate = nameof(FluentInjections),
+            InterfaceTypeName = nameof(IFluentOrchestrator<WebApplication>),
+            AttributeTypeName = nameof(AdapterMetadataAttribute)
+        };
+
+        public required string AssemblyFilterPredicate { get; set; }
+        public required string InterfaceTypeName { get; set; }
+        public required string AttributeTypeName { get; set; }
+    }
+}
