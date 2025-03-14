1ec349e2b131133c23d1f6430cbb0f793fdd10db
Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>
2025-03-14 15:39:36 +0100

    Updated Tests.Unit.Adapters.AspNetCore

---
+++ Tests/Unit/Adapters/AspNetCore/WebApplicationBuilderAdapterFixture.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) FluentInjections Project. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using FluentInjections.Adapters.AspNetCore;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FluentInjections.Tests.Units.Adapters.AspNetCore
+{
+    public class WebApplicationBuilderAdapterFixture
+    {
+        public WebApplicationBuilderAdapter Adapter { get; }
+        public ServiceCollection Services { get; }
+
+        public WebApplicationBuilderAdapterFixture()
+        {
+            Adapter = new WebApplicationBuilderAdapter();
+            Services = new ServiceCollection();
+        }
+
+        public void AddTestService()
+        {
+            Services.AddSingleton<ITestService, TestService>();
+        }
+
+        internal interface ITestService { }
+        internal class TestService : ITestService { }
+    }
+}
---
+++ Tests/Unit/Adapters/AspNetCore/WebApplicationBuilderAdapterTests.cs
@@ -0,0 +1,83 @@
+﻿// Copyright (c) FluentInjections Project. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using FluentAssertions;
+
+using FluentInjections.Adapters.AspNetCore;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace FluentInjections.Tests.Units.Adapters.AspNetCore
+{
+    public class WebApplicationBuilderAdapterTests
+    {
+        [Fact]
+        public async Task BuildAsync_ShouldReturnApplicationAdapter()
+        {
+            // Arrange
+            var fixture = new WebApplicationBuilderAdapterFixture();
+
+            // Act
+            var result = await fixture.Adapter.BuildAsync();
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<WebApplicationAdapter>();
+        }
+
+        [Fact]
+        public void ConfigureServices_ShouldAddServicesToBuilder()
+        {
+            // Arrange
+            var fixture = new WebApplicationBuilderAdapterFixture();
+            fixture.AddTestService();
+
+            // Act
+            fixture.Adapter.ConfigureServices(fixture.Services);
+
+            // Assert
+            var serviceProvider = (fixture.Adapter.InnerBuilder as WebApplicationBuilder)!.Services.BuildServiceProvider();
+            var testService = serviceProvider.GetService<WebApplicationBuilderAdapterFixture.ITestService>();
+            testService.Should().NotBeNull();
+            testService.Should().BeOfType<WebApplicationBuilderAdapterFixture.TestService>();
+        }
+
+        [Fact]
+        public async Task DisposeAsync_ShouldNotThrow_WhenWebApplicationIsDisposable()
+        {
+            // Arrange
+            var fixture = new WebApplicationBuilderAdapterFixture();
+
+            // Act
+            await fixture.Adapter.DisposeAsync();
+
+            // Assert
+            // No exception should be thrown.
+        }
+
+        [Fact]
+        public void Constructor_ShouldInitializeLoggingProviders()
+        {
+            // Arrange
+            var fixture = new WebApplicationBuilderAdapterFixture();
+
+            // Assert
+            var builderServices = (fixture.Adapter.InnerBuilder as WebApplicationBuilder)!.Services;
+            var serviceProvider = builderServices.BuildServiceProvider();
+            var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+            loggerFactory.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void InnerBuilder_ShouldReturnWebApplicationBuilder()
+        {
+            // Arrange
+            var fixture = new WebApplicationBuilderAdapterFixture();
+
+            // Assert
+            fixture.Adapter.InnerBuilder.Should().BeOfType<WebApplicationBuilder>();
+        }
+    }
+}
