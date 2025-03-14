4e0fe06202179814fe65e2d297a226b1a92ff957
Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>
2025-03-14 15:33:08 +0100

    Updated Core.Filters

---
+++ Src/FluentInjections/Core/Filters/PredicateAssemblyFilter.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) FluentInjections Project. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using FluentInjections.Core.Abstractions;
+
+using Microsoft.CodeAnalysis;
+
+using System.Reflection;
+
+namespace FluentInjections.Core.Filters
+{
+    public class PredicateAssemblyFilter : IAssemblyFilter
+    {
+        private readonly Func<Assembly, bool> _predicate;
+
+        public PredicateAssemblyFilter(Func<Assembly, bool> predicate)
+        {
+            _predicate = predicate;
+        }
+
+        public bool ShouldInclude(Assembly assembly) => _predicate(assembly);
+        public bool ShouldInclude(AssemblyName assemblyName) => true;
+        public bool ShouldInclude(AssemblyMetadata assemblyMetadata) => true;
+    }
+}
