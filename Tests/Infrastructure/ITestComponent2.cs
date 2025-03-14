74c5d493cb6d7dfd8f8c588ae6ce744ea5526c1d
Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>
2025-03-14 15:38:21 +0100

    Updated Tests.Infrastructure

--- Tests/Infrastructure/ITestComponent2.cs
+++ Tests/Infrastructure/ITestComponent2.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) FluentInjections Project. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using FluentInjections.Configurators;
+using FluentInjections.Abstractions;
 
 namespace FluentInjections.Tests.Infrastructure
 {
