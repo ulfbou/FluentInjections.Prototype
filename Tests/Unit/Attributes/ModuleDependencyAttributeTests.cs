b86c860d2bf0f4de4b6d41af7b675fae5ba2da53
Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>
2025-03-14 15:40:19 +0100

    Updated Tests.Units.Attributes

---
+++ Tests/Unit/Attributes/ModuleDependencyAttributeTests.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) FluentInjections Project. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using FluentAssertions;
+
+using FluentInjections.Attributes;
+
+namespace FluentInjections.Tests.Units.Attributes
+{
+    public class ModuleDependencyAttributeTests
+    {
+        [Fact]
+        public void Constructor_ShouldSetDependencyType()
+        {
+            // Arrange
+            var dependencyType = typeof(string);
+
+            // Act
+            var attribute = new ModuleDependencyAttribute(dependencyType);
+
+            // Assert
+            attribute.DependencyType.Should().Be(dependencyType);
+        }
+    }
+}
