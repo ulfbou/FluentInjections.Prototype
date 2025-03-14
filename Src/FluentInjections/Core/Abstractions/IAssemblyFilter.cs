3f85394c28a54fee5e9e96d9f055040808785868
Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>
2025-03-14 15:32:20 +0100

    Updated Core.Abstractions

---
+++ Src/FluentInjections/Core/Abstractions/IAssemblyFilter.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) FluentInjections Project. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Microsoft.CodeAnalysis;
+
+using System.Reflection;
+
+namespace FluentInjections.Core.Abstractions
+{
+    public interface IAssemblyFilter
+    {
+        bool ShouldInclude(Assembly assembly);
+        bool ShouldInclude(AssemblyName assemblyName);
+        bool ShouldInclude(AssemblyMetadata assemblyMetadata);
+    }
+}
---
+++ Src/FluentInjections/Core/Abstractions/ITypeDiscoveryStrategy.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) FluentInjections Project. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using FluentInjections.Core.Discovery;
+
+namespace FluentInjections.Core.Abstractions
+{
+    public interface ITypeDiscoveryStrategy
+    {
+        IAsyncEnumerable<Type> Discover(ITypeDiscoveryContext context, CancellationToken cancellationToken = default);
+    }
+}
