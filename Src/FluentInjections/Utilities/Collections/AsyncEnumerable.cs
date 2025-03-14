908ae6ca40489b0aa866523d39fe422ea2a41ffe
Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>
2025-03-14 15:37:04 +0100

    Updated Utilities.Collections

---
+++ Src/FluentInjections/Utilities/Collections/AsyncEnumerable.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) FluentInjections Project. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+
+namespace FluentInjections.Utilities.Collections
+{
+    internal class AsyncEnumerable
+    {
+        internal static async IAsyncEnumerable<T> Empty<T>()
+        {
+            await Task.CompletedTask;
+            yield break;
+        }
+    }
+}
