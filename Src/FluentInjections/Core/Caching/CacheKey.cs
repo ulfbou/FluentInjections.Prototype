f372c9dd59a0748f8a016003f26b493d2c4b6a27
Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>
2025-03-14 15:32:30 +0100

    Updated Core.Caching

---
+++ Src/FluentInjections/Core/Caching/CacheKey.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) FluentInjections Project. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using FluentInjections.Core.Abstractions;
+
+namespace FluentInjections.Core.Caching
+{
+    public class CacheKey
+    {
+        public required IAssemblyFilter AssemblyFilter { get; set; }
+        public required Type InterfaceType { get; set; }
+        public required Type AttributeType { get; set; }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(AssemblyFilter, InterfaceType, AttributeType);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is CacheKey other && other.AssemblyFilter == AssemblyFilter && other.InterfaceType == InterfaceType && other.AttributeType == AttributeType;
+        }
+    }
+}
---
+++ Src/FluentInjections/Core/Caching/TypeDiscoveryCache.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) FluentInjections Project. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Collections.Concurrent;
+
+namespace FluentInjections.Core.Caching
+{
+    public class TypeDiscoveryCache
+    {
+        private readonly ConcurrentDictionary<CacheKey, Lazy<Task<List<System.Type>>>> _cache = new();
+
+        public async Task<List<System.Type>> GetOrAddAsync(CacheKey key, Func<CacheKey, Task<List<System.Type>>> valueFactory)
+        {
+            var lazyResult = _cache.GetOrAdd(key, new Lazy<Task<List<System.Type>>>(() => valueFactory(key)));
+            return await lazyResult.Value;
+        }
+    }
+}
