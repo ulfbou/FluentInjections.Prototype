f0d3430e152674fca988a939653cd5ef0878095d
Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>
2025-03-14 17:21:27 +0100

    Initial commit FluentInjections.Tests.Integration.Configuration

---
+++ Tests/Integration/Configuration/ConfigurationDiscoveryIntegrationTests.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) FluentInjections Project. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using FluentAssertions;
+
+namespace FluentInjections.Tests.Integration.Configuration
+{
+    public class ConfigurationDiscoveryIntegrationTests
+    {
+        [Fact]
+        public async System.Threading.Tasks.Task BuildAsync_ShouldDiscoverModulesBasedOnConfiguration()
+        {
+            var fixture = new IntegrationTestFixture<TestApplication>();
+            var application = await fixture.Orchestrator.BuildAsync();
+
+            var serviceProvider = fixture.Services.BuildServiceProvider();
+
+            // Verify that TestModuleA is registered as ITestModule.
+            serviceProvider.GetService<ITestModule>().Should().BeOfType<TestModuleA>();
+
+            // Verify that TestModuleB is not registered.
+            serviceProvider.GetService<TestModuleB>().Should().BeNull();
+        }
+    }
+    public class TestApplication { }
+}
---
+++ Tests/Integration/Configuration/IntegrationTestFixture.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) FluentInjections Project. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Castle.Core.Configuration;
+using Castle.Core.Logging;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FluentInjections.Tests.Integration.Configuration
+{
+    public class IntegrationTestFixture<TApplication>
+    {
+        public IConfiguration Configuration { get; }
+        public ILoggerFactory LoggerFactory { get; }
+        public IServiceCollection Services { get; }
+        public FluentOrchestrator<TApplication> Orchestrator { get; }
+
+        public IntegrationTestFixture()
+        {
+            Configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.test.json")
+                .Build();
+
+            LoggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+
+            Services = new ServiceCollection();
+            Orchestrator = new FluentOrchestrator<TApplication>(null, Configuration, LoggerFactory, Services);
+        }
+    }
+}
