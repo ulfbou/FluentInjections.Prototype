9cec79ecb68846aca150e9a47687a8d1e5c833ea
Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>
2025-03-14 15:35:50 +0100

    Updated Registration

--- Src/FluentInjections/Registration/ServiceConfigurator.cs
+++ Src/FluentInjections/Registration/ServiceConfigurator.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using FluentInjections.Abstractions;
-using FluentInjections.Internal.Building;
 using FluentInjections.Registration;
 using FluentInjections.Validation;
 
