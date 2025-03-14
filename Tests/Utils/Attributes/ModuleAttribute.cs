90c2fd02153e04f4d66d71232c012f7cf64e088a
Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>
2025-03-14 17:31:23 +0100

    Initial commit

---
+++ Tests/Utils/Attributes/ModuleAttribute.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) FluentInjections Project. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+// Copyright (c) FluentInjections Project. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace FluentInjections.Tests.Utils.Attributes
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class ModuleAttribute : Attribute
+    {
+        public ModuleAttribute(string? name = null)
+        {
+            Name = name ?? string.Empty;
+        }
+
+        public string Name { get; }
+    }
+}
---
+++ Tests/Utils/Modules/TestModules.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) FluentInjections Project. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using FluentInjections.Tests.Utils.Attributes;
+
+namespace FluentInjections.Tests.Utils.Modules
+{
+    [Module("TestModuleA")]
+    public class TestModuleA { }
+    public class TestModulesB { }
+}
